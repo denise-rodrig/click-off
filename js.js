5de1cc4cd046ddd30733a1cbcc20dd8da1a91932
denise-rodrig <jobs.1243236@gmail.com>
2022-03-27 12:17:57 -0300

    atualizacoes de botoes

--- js.js
+++ js.js
@@ -2,3 +2,10 @@ function mudaStyles (bnt3){
     bnt3.classList.toggle("bntNewStyles");
     return
 };
+let bnt8=document.getElementById("bnt8");
+bnt8.addEventListener("click",mudaBorder)
+function mudaBorder(){
+    bnt8.classList.add("bnt8Animation")
+    bnt8.innerHTML="clicado com sucesso";
+    bnt8.style.color = "pink"
+}
